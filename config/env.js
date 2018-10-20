a9326e90e6f1598caab64461aaae49e15a2efee1
Gordon Yeh <gordon.yeh.ubc@gmail.com>
2018-10-20 09:50:21 -0700

    Modified: env.js to make DB route and password env variables

--- config/env.js
+++ config/env.js
@@ -9,12 +9,18 @@ function init() {
         env.ENV = 'development';
     }
 
+    if (!nodeEnv.DB_ROUTE) {
+        env.DB_ROUTE = 'test';
+    }  else {
+        env.DB_ROUTE = nodeEnv.DB_ROUTE;
+    }
+
     env.PORT = {
         production:  80,
         development: 3000,
     }[env.ENV];
 
-    env.DB_URL = 'mongodb+srv://diagram_server:M6qcdfrqt3jjv4Uq@cluster0-k8yai.mongodb.net/test?retryWrites=true';
+    env.DB_URL = `mongodb+srv://${nodeEnv.DB_NAME}:${nodeEnv.DB_PASS}@cluster0-k8yai.mongodb.net/${env.DB_ROUTE}?retryWrites=true`;
     
     return env;
 }
