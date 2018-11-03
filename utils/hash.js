fb61324ea58b5f36a5055a50eb6761c4f96b8b30
Gordon Yeh <gordon.yeh.ubc@gmail.com>
2018-11-03 16:56:46 -0700

    Added reference for hashing algorithm

--- utils/hash.js
+++ utils/hash.js
@@ -1,5 +1,6 @@
 var crypto = require('crypto');
 
+// reference: https://ciphertrick.com/2016/01/18/salt-hash-passwords-using-nodejs-crypto/
 function sha512(password, salt) {
     var hash = crypto.createHmac('sha512', salt); /** Hashing algorithm sha512 */
     hash.update(password);
