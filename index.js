a7baedb440cf40bc4963320bf1c1a8e0b4546522
Corwin Shanner <corwin.shanner@gmail.com>
2018-11-12 11:53:37 -0800

    compiles, test wip

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const app = require('./app.js');
+const envConfig = require('./config/env.js');
+
+const env = envConfig.get();
 
 app.listen(env.PORT, () => {
 	console.log(`Diagram API server listening on port ${env.PORT}!`);
